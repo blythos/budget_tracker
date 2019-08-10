2622e955c71bb56afc65a681b2c00a475ea24c1d
Stephen Blythe <stephenblythe@gmail.com>
2019-08-10 15:05:06 +0100

    Tests getters and setters in Transaction class.

---
+++ models/specs/transaction_spec.rb
@@ -0,0 +1,45 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('../Transaction.rb')
+
+class TestTransaction < MiniTest::Test
+
+  def setup()
+    @transaction1 = Transaction.new(
+      {
+        'id' => 1,
+        'amount' => 45,
+        'transaction_date' => '14/06/19',
+        'transaction_time' => '21:47',
+        'tag_id' => 67,
+        'merchant_id' => 12
+      }
+    )
+  end
+
+  def test_get_id()
+    assert_equal(1, @transaction1.id)
+  end
+
+  def test_get_amount()
+    assert_equal(45, @transaction1.amount)
+  end
+
+  def test_get_date()
+    assert_equal('14/06/19', @transaction1.transaction_date)
+  end
+
+  def test_get_time()
+    assert_equal('21:47', @transaction1.transaction_time)
+  end
+
+  def test_get_tag_id()
+    assert_equal(67, @transaction1.tag_id)
+  end
+
+  def test_get_merchant_id()
+    assert_equal(12, @transaction1.merchant_id)
+  end 
+
+end
