d023d25159d82e90f0690639fc5d6ba2b3383447
Stephen Blythe <stephenblythe@gmail.com>
2019-08-11 21:03:47 +0100

    Adds CREATE, EDIT, UPDATE, AND DESTROY functionality to /tags

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -6,6 +6,38 @@ get '/tags' do
   erb(:'/tags/index')
 end
 
+# CREATE
+post '/tags' do
+  tag = Tag.new(params)
+  tag.save()
+  redirect to ('/tags')
+end
+
+# CREATE
+get '/tags/new' do
+  erb(:'/tags/new')
+end
+
+# EDIT
+get '/tags/:id/edit' do
+  @tag = Tag.find(params['id'].to_i)
+  erb(:'/tags/edit')
+end
+
+# DESTROY
+post '/tags/:id/delete' do
+  tag = Tag.find(params['id'].to_i)
+  tag.delete()
+  redirect to ('/tags')
+end
+
+# UPDATE
+post '/tags/:id' do
+  tag = Tag.new(params)
+  tag.update()
+  redirect to ("/tags/#{tag.id}")
+end
+
 # SHOW
 get '/tags/:id' do
   @tag = Tag.find(params['id'].to_i)
