599cfbaf618c44449e962a532004c4081ecfe9cc
Stephen Blythe <stephenblythe@gmail.com>
2019-11-05 13:11:07 +0000

    quick bugfix on totals

--- models/Transaction.rb
+++ models/Transaction.rb
@@ -111,12 +111,23 @@ end
    return gbp
  end
 
+ # Quick fix while reworking gbp functions.
+  def self.gbp(amount)
+    amount_string = amount.to_s
+    pence = amount_string[-2, 2]
+    pounds = amount_string[0..-3]
+    gbp = "£#{pounds}.#{pence}"
+    return gbp
+  end
+
  def self.format_date(date)
    year = date[0..3]
    month = date[5..6]
    day = date[8..9]
    return "#{day}/#{month}/#{year}"
  end
+
+
 
 
 end
